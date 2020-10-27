9663a16937e5241b2776118c74ff582dfe1295c4
MrHumphrey1 <45724359+MrHumphrey1@users.noreply.github.com>
2020-10-27 18:45:51 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -5,17 +5,15 @@ client.on("ready", () => {
 })
 
 client.on("message", (msg) => {
-  if (msg.content === "$poop") {
+  if (msg.content === "it is literally tuesday bruv" || msg.content === "it is literally tuesday") {
+    msg.reply("IT IS LITERALLY FUCKING TUESDAY BRUV.")
+  }
+  if (msg.content === "poop") {
     msg.reply("you're a retard")
   }
-  if (msg.content === "$help") {
-	msg.reply("\nList of commands:\n- $poop\n- $roast")
-  }
-  if (msg.content === "$roast") {
-	msg.reply("You're a poopy head.")
-  }
-  if (msg.conteent === "pog"){
-    message.channel.send("pog", {files: ["Pogchamp.jpg"]});
+
+  if (msg.content === "pog") {
+    msg.reply("pog", {files: ["pogchamp.png"]});
   }
 })
-client.login("NzcwNjk1MzQ1MDk2ODg0MjY4.X5hT-Q.5zIXeOsuCEX_oCVUfewIygRI0Cc")
+client.login("NzcwNjk1MzQ1MDk2ODg0MjY4.X5hT-Q.o5AIRrBf7T2Jv1zJX8eI0VnmDsc")
